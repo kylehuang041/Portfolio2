05c7268acb9a1d887d1b12654c23cc475f48f38f
kylehuang041 <kylehuang041@gmail.com>
2024-08-20 01:58:14 -0700

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId={`${process.env.GOOGLE_ANALYTICS_ID}`} />
     </html>
   );
 }
