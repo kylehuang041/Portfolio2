42f88c9ac6d0b28ee3476d509aac224727364f16
kylehuang041 <kylehuang041@gmail.com>
2025-05-05 00:59:56 -0700

    fixed glarecard bug

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -7,7 +7,7 @@ const Experience = () => {
       <h2 className="text-center text-3xl font-bold m-10">Work Experience</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto max-w-xs sm:max-w-none sm:self-center">
         {workExperience.map((item, i) => (
-          <GlareCard key={i} className="p-5">
+          <GlareCard key={i} className="p-5 w-[390px] h-[500px]">
             <h2 className="text-lg font-bold break-words">{item.title}</h2>
             <h3 className="break-words">{item.company}</h3>
             <p className="text-sm text-gray-400 break-words">{item.date}</p>
@@ -15,7 +15,7 @@ const Experience = () => {
             {item.links && Object.entries(item.links).map(([label, url], index) => (
               <>
                 <br/>
-                <a key={index} href={url} target="_blank" rel="noopener noreferrer" className="break-words pointer-events-auto z-10">
+                <a key={index} href={url} target="_blank" rel="noopener noreferrer" className="break-words">
                   {label}: {url}
                 </a>
               </>
